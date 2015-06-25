2401cd29b475e2572e06fdf6caf1d819927a3826
Hubert Bullen <Hubert_99_bullen@hotmail.com>
2015-06-25 12:35:49 +0200

    /hello works.

---
+++ src/main/java/be/vdab/films/FilmController.java
@@ -0,0 +1,18 @@
+package be.vdab.films;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Hyuberuto on 25/06/15.
+ */
+@Controller
+public class FilmController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World";
+    }
+}
