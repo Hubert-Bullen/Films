3ac8a10af75c1942da79f57e04cbc1b14279d365
Hubert Bullen <Hubert_99_bullen@hotmail.com>
2015-06-25 13:38:54 +0200

    Added Film and repository

---
+++ src/main/java/be/vdab/films/FilmRepository.java
@@ -0,0 +1,13 @@
+package be.vdab.films;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Hyuberuto on 25/06/15.
+ */
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Integer> {
+
+
+}
